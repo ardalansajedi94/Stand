acf27e03b34d3ece237e4e5664c02c480014d39a
yashar <ebrahimpoor.yashar@gmail.com>
2018-10-29 19:34:43 +0330

    slider added

---
+++ src/Component/Logo.js
@@ -0,0 +1,15 @@
+import React , {Component} from 'react';
+import {CardImg} from "reactstrap";
+
+
+export default class Logo extends Component{
+    render(){
+        return(
+            <div>
+                <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+            </div>
+
+        )
+    }
+
+}
